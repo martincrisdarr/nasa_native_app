181f04af056d345eb2cf2b9a5910a93a28e59100
martin <martincrisdarr@gmail.com>
2024-05-09 18:03:50 -0300

    Project created with Header

---
+++ app/src/componentes/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Header = () => {
+  return (
+    <View>
+      <View>
+        <Text>Explore</Text>
+      </View>
+      <View></View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Header
